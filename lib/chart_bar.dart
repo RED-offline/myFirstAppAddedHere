f5673f8b552281579cdcb0a9886327d020cf9be4
Dumko Kachuk <dumkokachuk@Dumkos-MacBook-Pro.local>
2023-02-04 14:30:36 +0200

    first commit

--- lib/chart_bar.dart
+++ lib/chart_bar.dart
@@ -10,51 +10,49 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 15,
-            child: FittedBox(
-              child: Text('\$ ${amount!.toStringAsFixed(0)}'),
-            ),
+    return Column(
+      children: [
+        SizedBox(
+          height: 15,
+          child: FittedBox(
+            child: Text('\$ ${amount!.toStringAsFixed(0)}'),
           ),
-          const SizedBox(height: 5),
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.white),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            height: 100,
-            width: 15,
-            child: Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+        ),
+        const SizedBox(height: 5),
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.white),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          height: 100,
+          width: 15,
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                Container(
-                  alignment: Alignment.bottomCenter,
-                  child: FractionallySizedBox(
-                    heightFactor: percentageOfTotal,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: accentColor,
-                      ),
+              ),
+              Container(
+                alignment: Alignment.bottomCenter,
+                child: FractionallySizedBox(
+                  heightFactor: percentageOfTotal,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: accentColor,
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          const SizedBox(
-            height: 5,
-          ),
-          Text(label!),
-        ],
-      ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Text(label!),
+      ],
     );
   }
 }
